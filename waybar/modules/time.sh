bc7b8cde4b36e05407b2da91048129ce8214be7a
manav <manavjotsingh17@gmail.com>
2024-12-05 22:41:41 -0800

    added dot clock

---
+++ waybar/modules/time.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+hour=$(date +%-I)
+min=$(date +%-M)
+
+# Hour blocks
+hour_blocks=""
+for ((i=1; i<=hour/3; i++)); do
+    hour_blocks+=" ━━━━━━━━━━"
+done
+for ((i=1; i<=hour%3; i++)); do
+    hour_blocks+=" ━━━"
+done
+
+# Minute blocks
+min_blocks=""
+for ((i=1; i<=min/10; i++)); do
+    min_blocks+="━━━━━━ "
+done
+for ((i=1; i<=min%10/5; i++)); do
+    min_blocks+="━━ "
+done
+for ((i=1; i<=min%5; i++)); do
+    min_blocks+="╴"
+done
+
+hour_length=${#hour_blocks}
+min_length=${#min_blocks}
+
+if [ $hour_length -ne $min_length ]; then
+    if [ $hour_length -gt $min_length ]; then
+        padding_length=$((hour_length - min_length))
+        padding=""
+        for ((i=0; i<padding_length; i++)); do
+            padding+=" "
+        done
+        min_blocks="$min_blocks$padding"
+    else
+        padding_length=$((min_length - hour_length))
+        padding=""
+        for ((i=0; i<padding_length; i++)); do
+            padding+=" "
+        done
+        hour_blocks="$padding$hour_blocks"
+    fi
+fi
+
+echo "$hour_blocks  •  $min_blocks"
