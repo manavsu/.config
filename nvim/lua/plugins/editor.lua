7d3241ec79eacc437037cb7c29acfa73b79ca8dd
manav <manavjotsingh17@gmail.com>
2025-03-30 14:10:23 -0700

    updated config for md-render

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,17 @@
+return {
+  { "pocco81/auto-save.nvim" },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = {
+        icons = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
+        width = "block",
+        left_pad = 1,
+        right_pad = 1,
+      },
+      checkbox = {
+        enabled = true,
+      },
+    },
+  },
+}
