1a4464b82d2cdc017637d6c82258a70d56fc650d
manav <manavjotsingh17@gmail.com>
2025-04-01 18:47:56 -0700

    updated keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("i", "<C-BS>", "<C-w>", { desc = "Delete word", noremap = true })
+
+map("n", "<C-Right", "w", { desc = "Next word", noremap = true })
+map("n", "<C-Left>", "b", { desc = "Previous word", noremap = true })
